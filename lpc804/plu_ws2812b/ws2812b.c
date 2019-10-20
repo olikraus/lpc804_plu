1fb469bde74b13159e820d8617d3206044e02598
kraus <olikraus@gmail.com>
2019-10-20 14:14:20 +0200

    update ws2812

--- lpc804/plu_ws2812b/ws2812b.c
+++ lpc804/plu_ws2812b/ws2812b.c
@@ -47,44 +47,6 @@ void __attribute__ ((interrupt)) SysTick_Handler(void)
 
 /*=======================================================================*/
 
-/*
-  b0 <= PIO0_19;
-  b1 <= PIO0_20;
-  b2 <= PIO0_21;
-  b3 <= PIO0_22;
-
-*/
-void output_4bits(int x)
-{
-  if ( x&1)
-    LPC_IOCON->PIO0_19 &= ~(1<<6);
-  else
-    LPC_IOCON->PIO0_19 |= (1<<6);
- 
-/*  
-  x >>= 1;
-  
-  if ( x&1)
-    LPC_IOCON->PIO0_20 &= ~(1<<6);
-  else
-    LPC_IOCON->PIO0_20 |= (1<<6);
-  
-  x >>= 1;
-  
-  if ( x&1)
-    LPC_IOCON->PIO0_21 &= ~(1<<6);
-  else
-    LPC_IOCON->PIO0_21 |= (1<<6);
-  
-  x >>= 1;
-  if ( x&1)
-    
-    LPC_IOCON->PIO0_22 &= ~(1<<6);
-  else
-    LPC_IOCON->PIO0_22 |= (1<<6);
-    */
-}
-
 
 /*=======================================================================*/
 int __attribute__ ((noinline)) main(void)
@@ -99,50 +61,5 @@ int __attribute__ ((noinline)) main(void)
   SysTick_Config(main_clk/1000UL*(unsigned long)SYS_TICK_PERIOD_IN_MS);
 
   
-  GPIOInit();
-  Enable_Periph_Clock(CLK_IOCON);
-  Enable_Periph_Clock(CLK_SWM);
-  
-  
   plu();		/* plu() will enable GPIO0 clock */
-
-
-  /*
-    go <= PIO0_18;
-
-    PIO0_16 <= data;
-    PIO0_17 <= active;
-  */
-  GPIOSetDir( PORT0, 15, OUTPUT);
-
-  /* clear the go flag */
-  LPC_IOCON->PIO0_18 |= (1<<6);
-
-  for(;;)
-  {
-    LPC_IOCON->PIO0_18 &= ~(1<<6);
-    delay_system_ticks(200*2);
-    LPC_IOCON->PIO0_18 |= (1<<6);
-    delay_system_ticks(200*2);
-    
-    /*
-    while ( LPC_GPIO_PORT->B0[17] != 0 )
-      ;
-    output_4bits(0x5);
-    //LPC_GPIO_PORT->B0[18] = 1;
-    LPC_IOCON->PIO0_18 &= ~(1<<6);
-    while ( LPC_GPIO_PORT->B0[17] == 0 )
-      ;
-    //LPC_GPIO_PORT->B0[18] = 0;
-    LPC_IOCON->PIO0_18 |= (1<<6);
-    */
-    if ( sys_tick_irq_cnt & 1 )
-    {
-      LPC_GPIO_PORT->B0[15] = 0;
-    }
-    else
-    {
-      LPC_GPIO_PORT->B0[15] = 1;
-    }
-  }
 }
