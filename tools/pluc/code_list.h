712eb3c8791cd66efeadd52ae3c7afc219e6cad3
kraus <olikraus@gmail.com>
2019-10-29 10:16:07 +0100

    license and copyright cleanup

--- tools/pluc/code_list.h
+++ tools/pluc/code_list.h
@@ -4,7 +4,26 @@
   
   Description of the list of constraints and assigned codes; uses the graph of
   constraints described in "cg_graph.h"
-  
+
+  Copyright (C) 2019 Oliver Kraus (olikraus@gmail.com)
+
+  This file is part of "pluc".
+
+  "pluc" is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  "pluc" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with DGC; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA  
+
+
 */
 
 #ifndef _CODE_LIST_H
