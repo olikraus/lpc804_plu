ee972d6518defcb39a6d2317e91e8a694348645a
kraus <olikraus@gmail.com>
2019-11-02 18:20:19 +0100

    color change

--- lpc804/plu_ws2812b/ws2812b.c
+++ lpc804/plu_ws2812b/ws2812b.c
@@ -97,11 +97,147 @@ void spi_out(uint8_t *data, int cnt)
 
 }
 
+/*=======================================================================*/
+
+/* https://stackoverflow.com/questions/3018313/algorithm-to-convert-rgb-to-hsv-and-hsv-to-rgb-in-range-0-255-for-both */
+
+
+void hsv_to_rgb(uint8_t h, uint8_t s, uint8_t v, uint8_t *r, uint8_t *g, uint8_t *b)
+{
+    uint8_t region, remainder, p, q, t;
+
+    if (s == 0)
+    {
+        *r = v;
+        *g = v;
+        *b = v;
+        return ;
+    }
+
+    region = h / 43;
+    remainder = (h - (region * 43)) * 6; 
+
+    p = (v * (uint16_t)(255 - s)) >> 8;
+    q = (v * (uint16_t)(255 - ((s * remainder) >> 8))) >> 8;
+    t = (v * (uint16_t)(255 - ((s * (uint16_t)(255 - remainder)) >> 8))) >> 8;
+
+    switch (region)
+    {
+        case 0:
+            *r = v; *g = t; *b = p;
+            break;
+        case 1:
+            *r = q; *g = v; *b = p;
+            break;
+        case 2:
+            *r = p; *g = v; *b = t;
+            break;
+        case 3:
+            *r = p; *g = q; *b = v;
+            break;
+        case 4:
+            *r = t; *g = p; *b = v;
+            break;
+        default:
+            *r = v; *g = p; *b = q;
+            break;
+    }
+}
+
+/*=======================================================================*/
+
+/* color component slide */
+struct _ccs_t
+{
+  int16_t current;	/* contains the current color component */
+  int16_t start;
+  int16_t steps;
+  
+  int16_t dir;		/* 1 if start < end or -1 if start > end */
+  int16_t num;
+  int16_t quot;
+  
+  int16_t den;
+  int16_t rem;  
+  int16_t frac;
+};
+typedef struct _ccs_t ccs_t;
+
+
+/*
+  Setup change from "start" to "end" with a specified amount of "steps".
+  After calling this procedure, ccs->current will contain the "start" value.
+*/
+void ccs_init(ccs_t *ccs, int16_t start, int16_t end, int16_t steps)
+{
+  ccs->start = start;
+  ccs->num = end-start;
+  ccs->den = steps-1;
+  ccs->steps = steps;
+  ccs->dir = 1;
+  
+  ccs->quot = ccs->num / ccs->den;
+  if ( ccs->num < 0 )
+  {
+    ccs->num = -ccs->num;
+    ccs->dir = -1;
+  }
+  ccs->rem = ccs->num % ccs->den;
+  
+  ccs->frac = ccs->den/2;
+  ccs->current = start;
+}
+
+/*
+  Make one step towards the "end" value. 
+  ccs->current will contain the updated value.
+*/
+int16_t ccs_step(ccs_t *ccs)
+{
+  if ( ccs->steps == 0 ) 
+    return 0;
+  
+  ccs->current += ccs->quot;
+  ccs->frac += ccs->rem;
+  if ( ccs->frac >= ccs->den )
+  {
+    ccs->current += ccs->dir;
+    ccs->frac -= ccs->den;
+  }
+  ccs->steps--;
+  return 1;
+}
+
+/*
+  f(x) = (e-s)/(n-1) * x + s
+  current = (num / den)  * (pos / den)
+  
+  Seek to the specified "pos"ition.
+  "pos" must be between 0 and "end"-1
+*/
+void ccs_seek(ccs_t *ccs, int16_t pos)
+{
+  int16_t p;
+  ccs->current = ccs->quot;
+  ccs->current *= pos;
+  p = ccs->rem * pos  + ccs->den/2;
+  if ( ccs->dir >= 0 )
+    ccs->current += p / ccs->den;
+  else
+    ccs->current -= p / ccs->den;
+  ccs->frac = p % ccs->den;
+  ccs->current += ccs->start;
+}
+
+
 
 /*=======================================================================*/
 int __attribute__ ((noinline)) main(void)
 {
   uint8_t a[] = { 0, 0x3c, 0,  0, 0, 0x3c};
+  int h;
+  ccs_t ccs_v;
+  int is_v_up = 1;
   int i;
   /* call to the lpc lib setup procedure. This will set the IRC as clk src and main clk to 24 MHz */
   SystemInit(); 
@@ -118,10 +254,27 @@ int __attribute__ ((noinline)) main(void)
   spi_init();	
 
   delay_micro_seconds(50);
+  ccs_init(&ccs_v, 40, 100, 27);
+  is_v_up = 1;
   for(;;)
   {
-    a[0]++;
-    a[0] &= 255;
+    h+=2;	/* will turn arount at 255 */
+    hsv_to_rgb(h, 255, ccs_v.current, a+0, a+1, a+2);
+    hsv_to_rgb(h+20, 255, ccs_v.current, a+3, a+4, a+5);
+    if ( ccs_step(&ccs_v) == 0 )
+    {
+      if ( is_v_up )
+      {
+	ccs_init(&ccs_v, 100,40, 17);
+	is_v_up = 0;
+      }
+      else
+      {
+	ccs_init(&ccs_v, 40, 100, 17);
+	is_v_up = 1;
+      }
+    }
+
     spi_out(a, 6);
     
     /* LPC804 has might have up to two bytes in the queue, wait for their transmission */    
@@ -130,7 +283,7 @@ int __attribute__ ((noinline)) main(void)
     delay_micro_seconds(50);
     
     /* end user delay */
-    delay_micro_seconds(10000L);
+    delay_micro_seconds(20000L);
   }
   
 }
