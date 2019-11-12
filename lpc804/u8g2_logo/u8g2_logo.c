ecd57e8f5118336c1d4fca8e28d9e7fcd85a52c2
kraus <olikraus@gmail.com>
2019-11-12 22:28:21 +0100

    u8g2 hw i2c

--- lpc804/u8g2_logo/u8g2_logo.c
+++ lpc804/u8g2_logo/u8g2_logo.c
@@ -1,7 +1,10 @@
 
 #include <LPC8xx.h>
 #include <syscon.h>
+#include <iocon.h>
 #include <gpio.h>
+#include <swm.h>
+#include <i2c.h>
 #include <delay.h>
 #include "u8g2.h"
 
@@ -9,6 +12,8 @@
 /*=======================================================================*/
 /* u8x8cb.c */
 uint8_t u8x8_gpio_and_delay_lpc804_sw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8x8_gpio_and_delay_lpc804_only_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
+uint8_t u8x8_byte_lpc804_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr);
 
 
 /*=======================================================================*/
@@ -27,29 +32,165 @@ volatile uint32_t sys_tick_irq_cnt=0;
 void __attribute__ ((interrupt)) SysTick_Handler(void)
 {  
   sys_tick_irq_cnt++;
-  GPIOSetBitValue(PORT0, 15, (sys_tick_irq_cnt & 1) == 0?0:1);
 }
 
 
 /*=======================================================================*/
+/* 
+  replacement for ConfigSWM(uint32_t func, uint32_t port_pin) 
+  
+  Args:
+    fn: A function number, e.g. T0_MAT0, see swm.h
+    port: A port number for the GPIO port (0..30)
+
+*/
+void mapFunctionToPort(uint32_t fn, uint32_t port)
+{
+  /* first reset the pin assignment to 0xff (this is also the reset value */
+  LPC_SWM->PINASSIGN[fn/4] |= ((0xffUL)<<(8*(fn%4)));
+  /* then write the destination pin to it */
+  LPC_SWM->PINASSIGN[fn/4] &= ~((port^255UL)<<(8*(fn%4)));
+}
+/*=======================================================================*/
+
+
+void i2c_init(void)
+{
+  
+  //for(;;)
+  {
+    Enable_Periph_Clock(CLK_I2C0);
+    Do_Periph_Reset(RESET_I2C0);
+    /*
+      15 MHz, I2C Busclock 100MHz
+	CLKDIV = 74  
+    */
+    LPC_SYSCON->I2C0CLKSEL = 1;	/* main clock */
+    
+    LPC_I2C0->CLKDIV = 74;
+    LPC_I2C0->MSTTIME = 0;
+    
+    
+    LPC_I2C0->CFG = CFG_MSTENA;
+
+    /*
+    {
+      uint8_t buf[1] = { 0xaf };
+      i2c_write( 0x78, buf, 1);
+    }
+    */
+    
+  }  
+  
+}
+
+/*
+  return:
+    0: time out
+    1: all ok
+*/
+int i2c_wait(void)
+{
+  uint32_t cnt = 0;
+  int i = 0;
+  while(!(LPC_I2C0->STAT & STAT_MSTPEND) )
+  {
+    cnt++;
+    if ( cnt > (1<<17) )
+      return 0;
+  }
+  return 1;
+}
+
+#define I2C_OK 0
+#define I2C_TIMEOUT_PRE_ADR 1
+
+/* pending flag is active too long */
+/* root cause: */
+/* 1) SWM mapping was done after I2C subsystem enable (it must be done before) */
+/* 2) SCL line is permanently pulled down */
+#define I2C_TIMEOUT_POST_ADR 2
+
+#define I2C_TIMEOUT_TX 3
+
+/* there was an attempt to access the I2C bus, but the I2C subsystem is not ready */
+#define I2C_NOT_IDLE 4 
+/* transmission not available after sending the address with write request */
+/* root cause: 1) wrong address, client not available */
+/* 2) SDA line is mapped to an incorrect port (permanently pull up or pull down) */
+/* 3) SCL line is mapped to an incorrect port (permanently pull up) */
+#define I2C_NO_TX_POST_ADR 5
+
+/* transmission not available after sending data (maybe NACK by client) */
+#define I2C_NO_TX_POST_TX 6
+
+int i2c_write( uint8_t adr, uint8_t *buf, uint8_t len )
+{	
+  uint32_t i;
+
+  // Wait for MSTPENDING bit set in STAT register
+  if ( i2c_wait() == 0 )
+    return I2C_TIMEOUT_PRE_ADR;
+  if ((LPC_I2C0->STAT & MASTER_STATE_MASK) != I2C_STAT_MSTST_IDLE)
+    return I2C_NOT_IDLE;
+  
+  LPC_I2C0->MSTDAT = adr | 0;    					// Address with 0 for RWn bit (WRITE)
+  LPC_I2C0->MSTCTL = CTL_MSTSTART;										// Start the transaction by setting the MSTSTART bit to 1 in the Master control register.
+  if ( i2c_wait() == 0 )   // Wait for the address to be ACK'd
+    return I2C_TIMEOUT_POST_ADR;
+  if ((LPC_I2C0->STAT & MASTER_STATE_MASK) != I2C_STAT_MSTST_TX)
+    return I2C_NO_TX_POST_ADR;
+  
+  for ( i = 0; i < len; i++ ) 
+  {
+    LPC_I2C0->MSTDAT = buf[i];               // Send the data to the slave
+    LPC_I2C0->MSTCTL = CTL_MSTCONTINUE;                // Continue the transaction
+    if ( i2c_wait() == 0 )  // Wait for the address to be ACK'd
+      return I2C_TIMEOUT_TX;
+    if ((LPC_I2C0->STAT & MASTER_STATE_MASK) != I2C_STAT_MSTST_TX)
+      return I2C_NO_TX_POST_TX;
+  }
+  
+  LPC_I2C0->MSTCTL = CTL_MSTSTOP;                    // Send a stop to end the transaction
+  return I2C_OK;
+}
+
+void i2c_out_blink_code(int err)
+{
+  int i;
+  for(;;)
+  {
+    for( i = 0; i < err; i++ )
+    {
+      GPIOSetBitValue(PORT0, 15, 1);
+      delay_micro_seconds(20000);
+      GPIOSetBitValue(PORT0, 15, 0);
+      delay_micro_seconds(400000);
+    }
+    delay_micro_seconds(1000000);
+  }
+}
+
+/*=======================================================================*/
 
 u8g2_t u8g2;
+static uint16_t offset = 0;
 
 void draw(u8g2_t *u8g2)
 {
     u8g2_SetFontMode(u8g2, 1);	// Transparent
     u8g2_SetFontDirection(u8g2, 0);
     u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-    u8g2_DrawStr(u8g2, 0, 30, "U");
+    u8g2_DrawStr(u8g2, 0+offset, 30, "U");
     
     u8g2_SetFontDirection(u8g2, 1);
     u8g2_SetFont(u8g2, u8g2_font_inb30_mn);
-    u8g2_DrawStr(u8g2, 21,8,"8");
+    u8g2_DrawStr(u8g2, 21+offset,8,"8");
         
     u8g2_SetFontDirection(u8g2, 0);
     u8g2_SetFont(u8g2, u8g2_font_inb24_mf);
-    u8g2_DrawStr(u8g2, 51,30,"g");
-    u8g2_DrawStr(u8g2, 67,30,"\xb2");
+    u8g2_DrawStr(u8g2, 51+offset,30,"g");
+    u8g2_DrawStr(u8g2, 67+offset,30,"\xb2");
     
     u8g2_DrawHLine(u8g2, 2, 35, 47);
     u8g2_DrawHLine(u8g2, 3, 36, 47);
@@ -67,6 +208,8 @@ void draw(u8g2_t *u8g2)
 */
 int __attribute__ ((noinline)) main(void)
 {
+  uint8_t cnt = 0;
+  int err;
 
   /* call to the lpc lib setup procedure. This will set the IRC as clk src and main clk to 24 MHz */
   SystemInit(); 
@@ -83,17 +226,40 @@ int __attribute__ ((noinline)) main(void)
   /* enable clock for several subsystems */
   Enable_Periph_Clock(CLK_IOCON);
   Enable_Periph_Clock(CLK_SWM);
-  
+
   /* systick alive blink */
   GPIOSetDir( PORT0, 15, OUTPUT);
+  LPC_IOCON->PIO0_9 =  IOCON_RESERVED789 | MODE_PULLUP;
+  
+  LPC_IOCON->PIO0_14 |= 1<<IOCON_OD;
+  LPC_IOCON->PIO0_7 |= 1<<IOCON_OD;
+  mapFunctionToPort(I2C0_SDA, 14);
+  mapFunctionToPort(I2C0_SCL, 7);
+
+  i2c_init();
+
+  //LPC_IOCON->PIO0_14 |= 1<<IOCON_OD;
+  //LPC_IOCON->PIO0_7 |= 1<<IOCON_OD;
+  
+  {
+    uint8_t buf[1] = { 0x0af };
+    err = i2c_write(0x078, buf, 1);
+  }
+  if ( err > 0 )
+    i2c_out_blink_code(err);
+  
+  //GPIOSetDir( PORT0, 14, OUTPUT);
+  //GPIOSetDir( PORT0, 7, OUTPUT);
+
 
   /* setup display */
-  u8g2_Setup_ssd1306_i2c_128x64_noname_2(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_lpc804_sw_i2c);
+  /*
+  u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_lpc804_sw_i2c);
+  */
+  u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_lpc804_i2c, u8x8_gpio_and_delay_lpc804_only_delay);
   u8g2_InitDisplay(&u8g2);
   u8g2_SetPowerSave(&u8g2, 0);
 
-  
-  
   for(;;)
   {
     u8g2_FirstPage(&u8g2);
@@ -102,7 +268,11 @@ int __attribute__ ((noinline)) main(void)
       draw(&u8g2);
     } while( u8g2_NextPage(&u8g2) );
     
-    delay_micro_seconds(1000000);
+    //GPIOSetBitValue(PORT0, 15, (cnt & 1) == 0?0:1);
+    cnt++;
+    offset++;
+    offset &=7;
+    //delay_micro_seconds(1000000);
   }
 }
 
--- lpc804/u8g2_logo/u8x8cb.c
+++ lpc804/u8g2_logo/u8x8cb.c
@@ -1,11 +1,22 @@
 /*
   u8x8cb.c
+  
+  
+  
+  u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_sw_i2c, u8x8_gpio_and_delay_lpc804_sw_i2c);
+  u8g2_Setup_ssd1306_i2c_128x64_noname_1(&u8g2, U8G2_R0, u8x8_byte_lpc804_i2c, u8x8_gpio_and_delay_lpc804_only_delay);
+    
 */
 
 #include "LPC8xx.h"
 #include "gpio.h"
 #include "delay.h"
+#include "i2c.h"
 #include "u8x8.h"
+
+
+int i2c_write( uint8_t adr, uint8_t *buf, uint8_t len );
+
 
 /* although we use SW I2C, use the HW pins for later upgrade */
 #define I2C_CLOCK_PIN 7
@@ -83,6 +94,88 @@ uint8_t u8x8_gpio_and_delay_lpc804_sw_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg
     default:
       u8x8_SetGPIOResult(u8x8, 1);
       break;
+  }
+  return 1;
+}
+
+
+uint8_t u8x8_gpio_and_delay_lpc804_only_delay(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  switch(msg)
+  {
+    case U8X8_MSG_GPIO_AND_DELAY_INIT:
+      break;
+    case U8X8_MSG_DELAY_NANO:
+      /* not required for SW I2C */
+      break;
+    
+    case U8X8_MSG_DELAY_10MICRO:
+      delay_micro_seconds(arg_int*10UL);
+      break;
+    
+    case U8X8_MSG_DELAY_100NANO:
+      delay_micro_seconds(arg_int/10UL);
+      break;
+   
+    case U8X8_MSG_DELAY_MILLI:
+      delay_micro_seconds(arg_int*1000UL);
+      break;
+    case U8X8_MSG_DELAY_I2C:
+      /* arg_int is 1 or 4: 100KHz (5us) or 400KHz (1.25us) */
+      delay_micro_seconds(arg_int<=2?5:1);
+      break;
+  }
+  return 1;    
+}
+
+
+
+
+/*
+  template from https://github.com/olikraus/u8g2/wiki/Porting-to-new-MCU-platform
+
+  assumes that the I2C0 outputs are mapped to the pins:
+    mapFunctionToPort(I2C0_SDA, 14);
+    mapFunctionToPort(I2C0_SCL, 7);
+
+
+*/
+uint8_t u8x8_byte_lpc804_i2c(u8x8_t *u8x8, uint8_t msg, uint8_t arg_int, void *arg_ptr)
+{
+  /* u8g2/u8x8 will never send more than 32 bytes between START_TRANSFER and END_TRANSFER */
+  /* add extra byte for the i2c address */
+  static uint8_t buffer[34];		
+  static uint8_t buf_idx;
+  uint8_t *data;
+ 
+  switch(msg)
+  {
+    case U8X8_MSG_BYTE_SEND:
+      data = (uint8_t *)arg_ptr;      
+      while( arg_int > 0 )
+      {
+	buffer[buf_idx++] = *data;
+	data++;
+	arg_int--;
+      }      
+      break;
+    case U8X8_MSG_BYTE_INIT:
+      /* add your custom code to init i2c subsystem */
+      break;
+    case U8X8_MSG_BYTE_SET_DC:
+      /* ignored for i2c */
+      break;
+    case U8X8_MSG_BYTE_START_TRANSFER:
+      buf_idx = 0;
+      break;
+    case U8X8_MSG_BYTE_END_TRANSFER:
+      //buffer[0] = u8x8_GetI2CAddress(u8x8);
+      //I2CmasterWrite( buffer, buf_idx-1);
+      i2c_write(u8x8_GetI2CAddress(u8x8), buffer, buf_idx);
+      //i2c_write(0x078, buffer, buf_idx);
+      break;
+    default:
+      return 0;
   }
   return 1;
 }
